ede421739cac0df3bafd37214cfd4f3c380595a4
TIMURishche <88502822+TIMURishche@users.noreply.github.com>
2025-03-07 17:49:44 +0200

    Moved channel arguments to discord package

--- api/src/main/java/net/survivalboom/sbds/api/commands/argument/discord/channel/TextChannelArgument.java
+++ api/src/main/java/net/survivalboom/sbds/api/commands/argument/discord/channel/TextChannelArgument.java
@@ -1,4 +1,4 @@
-package net.survivalboom.sbds.api.commands.argument.channel;
+package net.survivalboom.sbds.api.commands.argument.discord.channel;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
