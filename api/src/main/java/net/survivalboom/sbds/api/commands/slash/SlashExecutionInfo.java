4219291317fde40b27623c9f74b45468a12a8527
TIMURishche <88502822+TIMURishche@users.noreply.github.com>
2025-03-07 17:51:43 +0200

    Added methods to get Guild and GuildMember from command execution (instead of writing every time interaction().guild())

--- api/src/main/java/net/survivalboom/sbds/api/commands/slash/SlashExecutionInfo.java
+++ api/src/main/java/net/survivalboom/sbds/api/commands/slash/SlashExecutionInfo.java
@@ -1,5 +1,7 @@
 package net.survivalboom.sbds.api.commands.slash;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import net.survivalboom.sbds.api.ISBDS;
@@ -7,26 +9,32 @@ import net.survivalboom.sbds.api.commands.Command;
 import net.survivalboom.sbds.api.commands.ExecutionInfo;
 import net.survivalboom.sbds.api.utils.TypeMap;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 public class SlashExecutionInfo extends ExecutionInfo {
 
     private final SlashCommandInteraction interaction;
 
-    private final User user;
-
     public SlashExecutionInfo(@NotNull Command command, @NotNull SlashCommandInteraction interaction, @NotNull String alias, @NotNull TypeMap arguments, @NotNull Logger logger, @NotNull ISBDS sbds) {
         super(command, alias, arguments, logger, sbds);
         this.interaction = interaction;
-        this.user = interaction.getUser();
     }
 
     public @NotNull SlashCommandInteraction interaction() {
         return interaction;
     }
 
+    public @Nullable Guild guild() {
+        return this.interaction.getGuild();
+    }
+
+    public @Nullable Member guildMember() {
+        return this.interaction.getMember();
+    }
+
     public @NotNull User user() {
-        return user;
+        return this.interaction.getUser();
     }
 
 }
