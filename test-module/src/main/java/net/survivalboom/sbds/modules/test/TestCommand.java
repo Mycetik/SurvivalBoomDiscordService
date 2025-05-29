94e97b836dd5c419f71afd4661c9edf83f489bcf
TIMURishche <88502822+TIMURishche@users.noreply.github.com>
2025-05-29 22:08:40 +0300

    Тестова команда для перевірки button.

--- test-module/src/main/java/net/survivalboom/sbds/modules/test/TestCommand.java
+++ test-module/src/main/java/net/survivalboom/sbds/modules/test/TestCommand.java
@@ -1,53 +1,33 @@
 package net.survivalboom.sbds.modules.test;
 
-import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
-import net.survivalboom.sbds.api.commands.argument.Argument;
-import net.survivalboom.sbds.api.commands.argument.primitive.StringArgument;
 import net.survivalboom.sbds.api.commands.base.Command;
-import net.survivalboom.sbds.api.commands.base.CommandArgument;
 import net.survivalboom.sbds.api.commands.base.CommandBase;
 import net.survivalboom.sbds.api.commands.slash.SlashCommand;
 import net.survivalboom.sbds.api.commands.slash.SlashExecutionInfo;
-import net.survivalboom.sbds.api.interaction.modal.IModalInteractionManager;
-import net.survivalboom.sbds.api.interaction.modal.ModalTemplate;
+import net.survivalboom.sbds.api.interaction.button.ButtonInteractionInfo;
 import net.survivalboom.sbds.api.modules.ModuleMain;
 import org.jetbrains.annotations.NotNull;
 
-@Command(name = "test", description = "Рисует большой жЫрный член.", permission = "testmodule.command.testcommand", defaultPermission = true)
+@Command(name = "test", description = "Тут має бути текст", permission = "testmodule.command.testcommand", defaultPermission = true)
 public class TestCommand extends CommandBase implements SlashCommand {
-
-    private final IModalInteractionManager.IRegisteredModal modal;
 
     public TestCommand(ModuleMain moduleMain) {
 
-        ModalTemplate modal = ModalTemplate.builder()
-                .setTitle("Уведіть номер вашої картки.")
-                .addInput("number", "Номер картки", "0000 0000 0000 0000", TextInputStyle.SHORT)
-                .addInput("expiry", "Термін дії", "3/12/2016", TextInputStyle.SHORT)
-                .addInput("cvv2", "CVV2", "678", TextInputStyle.SHORT)
-                .build();
-
-        this.modal = moduleMain.getSbds().getModalInteractionManager().registerModal(moduleMain, "test", modal);
+        moduleMain.getSbds().getButtonInteractionManager().registerButton(moduleMain.getModule(), "ban", this::button);
 
     }
 
     @Override
     public void executes(@NotNull SlashExecutionInfo info) {
 
-        modal.open(info.interaction(), null).thenAccept(modal -> {
-            modal.reply(modal.values().toString()).queue();
-        }).get;
+        info.reply("commands.sbds.status").queue();
 
     }
 
-    @CommandArgument(name = "key", required = false)
-    public Argument<?> key() {
-        return new StringArgument();
-    }
+    private void button(@NotNull ButtonInteractionInfo info) {
 
-    @CommandArgument(name = "value", required = false)
-    public Argument<?> value() {
-        return new StringArgument();
+        info.replyRaw("ТЫ ЕБЛАААААН, ТЫ ЗАБАНЕН БЛЯЯЯЯТЬ, Я НЕ БУДУ ТЕБЯ РАЗБАНИВАТЬ, ПОТОМУ ЧТО ТЫ ТУПОЙ СУКА!!!").queue();
+
     }
 
 }
