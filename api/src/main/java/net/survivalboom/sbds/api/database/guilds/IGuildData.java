0a75e0727a2db7c245f03869c542c3fa6e23d30a
TIMURishche <88502822+TIMURishche@users.noreply.github.com>
2025-06-04 05:22:09 +0300

    Забув закомітити...

--- api/src/main/java/net/survivalboom/sbds/api/database/guilds/IGuildData.java
+++ api/src/main/java/net/survivalboom/sbds/api/database/guilds/IGuildData.java
@@ -1,13 +1,17 @@
 package net.survivalboom.sbds.api.database.guilds;
 
+import net.survivalboom.sbds.api.translations.ITranslation;
 import net.survivalboom.sbds.api.utils.NamespacedContainer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface IGuildData {
 
     long getId();
 
     @NotNull NamespacedContainer container();
+
+    @Nullable ITranslation translation();
 
     void save();
 
