e6315af195228880cad58d6f5cf14f4711b29457
Mycetik <120984956+Mycetik@users.noreply.github.com>
2026-06-11 00:34:10 +0300

    Додано команду /modules restart

---
+++ core/src/main/java/net/survivalboom/sbds/core/commands/cmds/console/modules/RestartModuleCommand.java
@@ -0,0 +1,48 @@
+package net.survivalboom.sbds.core.commands.cmds.console.modules;
+
+import net.survivalboom.sbds.api.commands.argument.Argument;
+import net.survivalboom.sbds.api.commands.argument.sbds.ModuleArgument;
+import net.survivalboom.sbds.api.commands.base.ArgumentMethod;
+import net.survivalboom.sbds.api.commands.base.CommandBase;
+import net.survivalboom.sbds.api.commands.base.CommandClass;
+import net.survivalboom.sbds.api.commands.console.ConsoleCommandExecutor;
+import net.survivalboom.sbds.api.commands.console.ConsoleExecutionInfo;
+import net.survivalboom.sbds.api.modules.IModule;
+import net.survivalboom.sbds.api.modules.ModuleRefusedException;
+import net.survivalboom.sbds.api.modules.ModuleStateCallbackException;
+import org.jetbrains.annotations.NotNull;
+
+@CommandClass(name = "restart")
+public class RestartModuleCommand extends CommandBase implements ConsoleCommandExecutor {
+
+    @Override
+    public void executes(@NotNull ConsoleExecutionInfo info) {
+
+        IModule module = info.arguments().getCast("module", IModule.class).orElseThrow();
+
+        try {
+            info.sbds().getModuleManager().disableModule(module);
+        }
+
+        catch (ModuleStateCallbackException e) {
+            info.logger().error("Failed to disable module properly! An exception was thrown.", e);
+        }
+
+        try {
+            info.sbds().getModuleManager().enableModule(module);
+        }
+
+        catch (ModuleStateCallbackException | ModuleRefusedException e) {
+            info.logger().error("Failed to enable module `{}`. An exception was thrown.", module.getName(), e);
+            return;
+        }
+
+        info.logger().info("Successfully restarted module `{}`.", module.getName());
+    }
+
+    @ArgumentMethod
+    public Argument<?> module() {
+        return new ModuleArgument(true);
+    }
+
+}
